4a3a8a2a8a50d3b2137f7109fe64a170d5b46cae
Mario Johnson <marioj@Marios-MBP.attlocal.net>
2019-06-17 20:09:18 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -3,6 +3,8 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
 
+
+function sayHello() {
+    return '';
 }
--- tests.js
+++ tests.js
@@ -19,4 +19,13 @@ describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function() {
+        expect(typeof sayHello).toBe("string");
+    });
+    it('should return a string when executed', function() {
+        expect(sayHello()).toBe("");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(sayHello()).not.toBe(undefined);
+    });
 });
